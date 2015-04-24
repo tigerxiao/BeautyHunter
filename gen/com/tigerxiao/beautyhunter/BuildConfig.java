d57cb9ccdcdad5856c3017198c2786e81090b547
tigerxiao <xiaowh001@126.com>
2015-04-24 21:39:16 +0800

    理解Activity的生命周期

---
+++ gen/com/tigerxiao/beautyhunter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tigerxiao.beautyhunter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/tigerxiao/basic/ActivityLifeCycle.java
@@ -0,0 +1,72 @@
+package com.tigerxiao.basic;
+
+import com.tigerxiao.beautyhunter.R;
+import com.tigerxiao.beautyhunter.R.layout;
+import com.tigerxiao.extern.util.L;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityLifeCycle extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		L.i("onCreate");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		L.d("onStart");
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		L.d("onRestart");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		L.d("onResume");
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		L.d("onSave");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		L.d("onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		L.d("onStop");
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		L.d("onDestroy");
+	}
+
+
+	
+	
+}
