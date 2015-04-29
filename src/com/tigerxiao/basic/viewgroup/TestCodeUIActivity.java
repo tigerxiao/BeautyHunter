337fe9872c1e9085442364ac50f65543d81a6fc1
TigerXiao1122 <xiaowh001@126.com>
2015-04-29 11:10:03 +0800

    如何通过编程的方式修改UI

---
+++ src/com/tigerxiao/basic/viewgroup/TestCodeUIActivity.java
@@ -0,0 +1,39 @@
+package com.tigerxiao.basic.viewgroup;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class TestCodeUIActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+	//create params
+	LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT);
+	 
+	//create a layout
+	LinearLayout layout = new LinearLayout(this);
+	layout.setOrientation(LinearLayout.VERTICAL);
+	
+	//create edittext
+	EditText editText = new EditText(this);
+	editText.setHint("这是文本框");
+	editText.setLayoutParams(layoutParams);
+	
+	Button button = new Button(this);
+	button.setText("这是一个按钮");
+	button.setLayoutParams(layoutParams);
+	
+	layout.addView(editText);
+	layout.addView(button);
+	
+	this.addContentView(layout, layoutParams);
+	}
+
+}
